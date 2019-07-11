697ecbeeb1b698bdee578c9f285bb71d3e389d99
adamzul <adamzulkarnain98@gmail.com>
2019-07-11 16:08:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import {Provider} from 'react-redux';
 
 import Posts from './components/Posts';
 import Postform from './components/Postform';
+import store from './store';
 function App() {
   return (
-    <Provider store=
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -27,6 +28,7 @@ function App() {
       <hr/>
       <Posts />
     </div>
+    </Provider>
   );
 }
 
